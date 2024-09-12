e7345d27db3dfa9d32310270e4819287f813aa0d
a-mt <amistri@live.fr>
2024-09-12 12:45:19 +0200

    MAJ reducer

--- reducer_projet.py
+++ reducer_projet.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 
 # Dictionnaire pour stocker les informations par client
-clients = defaultdict(lambda: {'quantite_totale': 0, 'fidelite_totale': 0, 'objets': defaultdict(int)})
+clients = defaultdict(lambda: {'totalPoint': 0, 'objets': defaultdict(int)})
 
 # Fonction pour afficher les 10 meilleurs clients
 def get_top_clients(clients):
     # Trier les clients par fidélité décroissante et sélectionner les 10 premiers
-    return sorted(clients.items(), key=lambda x: x[1]['fidelite_totale'], reverse=True)[:10]
+    return sorted(clients.items(), key=lambda x: x[1]['totalPoint'], reverse=True)[:10]
 
 # Parcourir les données ligne par ligne depuis le mapper
 for line in sys.stdin:
@@ -18,15 +18,17 @@ for line in sys.stdin:
 
     # Séparer la clé (client) et la valeur (Objet, Quantité, Fidélité)
     client, value = line.split('\t')
-    lbobj, qty, points = value.split(',')
+    dpt, ville, lbobj, qte, points = value.split(',')
 
-    quantite = int(qty)
+    quantite = int(qte)
     fidelite = int(points)
 
     # Accumuler les quantités et fidélités totales pour chaque client
-    clients[client]['quantite_totale'] += quantite
-    clients[client]['fidelite_totale'] += fidelite
-    clients[client]['objets'][lbobj] += quantite  # Stocker tous les objets et leurs quantités
+    totalPoint = quantite * fidelite
+    clients[client]['totalPoint'] += totalPoint
+    #clients[client]['fidelite_totale'] += fidelite
+    #clients[client]['objets'][lbobj] += quantite  # Stocker tous les objets et leurs quantités
+
 
 # Obtenir les 10 clients les plus fidèles
 top_clients = get_top_clients(clients)
@@ -35,14 +37,14 @@ top_clients = get_top_clients(clients)
 data = []
 for client, info in top_clients:
     nomcli, prenomcli, villecli, dpt = client.split(',')
-    for lbobj, qty in info['objets'].items():
-        data.append([nomcli, prenomcli, villecli, dpt, lbobj, qty, info['fidelite_totale']])
+    for lbobj, qte in info['objets'].items():
+        data.append([nomcli, prenomcli, villecli, dpt, lbobj, qte, info['totalPoint']])
 
 # Créer un DataFrame Pandas
 df = pd.DataFrame(data, columns=['Nom', 'Prénom', 'Ville', 'Département', 'Objet', 'Quantité', 'Fidélité totale'])
 
 # Exporter le DataFrame dans un fichier Excel
-df.to_excel('top_clients_fideles.xlsx', index=False)
+df.to_excel('/datavolume1/top_clients_fideles.xlsx', index=False)
 
 # Générer des graphiques en PDF pour les 10 clients
 for client, info in top_clients:
@@ -55,7 +57,7 @@ for client, info in top_clients:
     plt.title(f"Répartition des objets commandés pour {nomcli} {prenomcli}")
 
     # Sauvegarder chaque graphique dans un fichier PDF distinct
-    plt.savefig(f"{nomcli}_{prenomcli}_repartition_objets.pdf")
+    plt.savefig(f"/datavolume1/{nomcli}_{prenomcli}_repartition.pdf")
     plt.close()
 
-print("Exportation Excel et graphiques PDF terminés.")
+print("OK")
