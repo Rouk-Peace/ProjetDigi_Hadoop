b8935d7655583ad087c03199d14d0f1b07208241
Rouk-Peace <orouk10@gmail.com>
2024-09-13 12:37:32 +0200

    mise à jour du mapper en commentant les stderr car cette version fonctionne bien

--- mapper_projet.py
+++ mapper_projet.py
@@ -30,42 +30,42 @@ for columns in reader:
 
     # verification des colonnes extraites pour s'assurer qu'elles ne sont pas vides ou Null
     if any(invalid(val) for val in [nom, prenom]):
-        sys.stderr.write("Ignored due to invalid data: %s\n" % str(columns))
+        #sys.stderr.write("Ignored due to invalid data: %s\n" % str(columns))
         continue  # ignore si une valeur pertinente est vide ou null
 
     # verification du département, extraction des deux premiers caractères du cpville
     if len(dpt) >= 5:
         dpt = dpt[:2]
     else:
-        sys.stderr.write("Ignored due to invalid department format: %s\n" % dpt)
+        #sys.stderr.write("Ignored due to invalid department format: %s\n" % dpt)
         continue
 
     # verification et conversion de la quantité en entier
     try:
         qte = int(qte)
     except ValueError:
-        sys.stderr.write("Ignored due to invalid quantity: %s\n" % qte)
+        #sys.stderr.write("Ignored due to invalid quantity: %s\n" % qte)
         continue
 
     # verification et conversion de la quantité en entier
     try:
         points = int(points)
     except ValueError:
-        sys.stderr.write("Ignored due to invalid points: %s\n" % points)
+        #sys.stderr.write("Ignored due to invalid points: %s\n" % points)
         continue
 
     # Extraire l'année de la date formatée
     try:
         year = int(date.split('-')[0])
     except ValueError:
-        sys.stderr.write("Ignored due to invalid date: %s\n" % date)
+        #sys.stderr.write("Ignored due to invalid date: %s\n" % date)
         continue  # Ignore les lignes si l'année ne peut pas être extraite
 
     # Filtrer les données : entre 2008 et 2012 et pour les départements spécifiques
     if dpt in ['53', '61', '75', '28'] and 2008 <= year <= 2012:
         # Émettre la clé (nom prenom) et les valeurs pertinentes
-        print("%s %s\t,%s,%s,%s,%i,%i" % (nom.lower(), prenom.lower(), dpt, ville.lower(), lbobj.lower(), qte, points))
-    else:
-        sys.stderr.write("Ignored due to filter conditions: %s\n" % str(columns))
+        print("%s %s\t%s,%s,%s,%i,%i" % (nom.lower(), prenom.lower(), dpt, ville.lower(), lbobj.lower(), qte, points))
+    #else:
+       # sys.stderr.write("Ignored due to filter conditions: %s\n" % str(columns))
 
 
